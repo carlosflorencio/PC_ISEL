295af8b9d80752002bd349d2179111d8bdd1c6f4
Carlos Florencio <carlosmflorencio@gmail.com>
2017-01-19 19:05:33 +0000

    Serie1-1

---
+++ Serie1/Serie1/RetryLazy.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Serie1 {
+    public class RetryLazy<T> where T : class {
+        private readonly Func<T> _provider;
+        private int _maxRetries;
+        private T _result;
+
+        //signaling
+        private readonly object _lock = new object();
+        private readonly LinkedList<int> _queue = new LinkedList<int>();
+        private bool _isBeingResolved = false;
+
+        public RetryLazy(Func<T> provider, int maxRetries) {
+            this._provider = provider;
+            this._maxRetries = maxRetries;
+        }
+
+        public T Value {
+            get {
+                lock (_lock) {
+                    if (_result != null)
+                        return _result;
+
+                    if (_maxRetries == 0)
+                        throw new InvalidOperationException();
+
+
+                    if (_isBeingResolved) {
+                        var node = _queue.AddLast(Thread.CurrentThread.ManagedThreadId);
+
+                        do {
+                            try {
+                                Monitor.Wait(_lock);
+
+                                if (node == _queue.First && _isBeingResolved == false) {
+                                    _queue.Remove(node);
+                                    break;
+                                }
+                                    
+                            } catch (ThreadInterruptedException) {
+                                _queue.Remove(node);
+                                ConditionalNotifyAll();
+                                throw;
+                            }
+                        } while (true);
+                        
+
+                        if (_result != null) {
+                            ConditionalNotifyAll();
+                            return _result;
+                        }
+
+
+                        if (_maxRetries == 0) {
+                            ConditionalNotifyAll();
+                            throw new InvalidOperationException();
+                        }
+                            
+                    }
+
+                    _isBeingResolved = true;
+                }
+
+                T temp;
+                try {
+                    temp = _provider();
+                } catch (Exception) {
+                    lock (_lock) {
+                        _maxRetries--;
+                        _isBeingResolved = false;
+                        ConditionalNotifyAll();
+                        throw;
+                    }
+                }
+
+                lock (_lock) {
+                    _result = temp;
+                    _isBeingResolved = false;
+
+                    ConditionalNotifyAll();
+
+                    return _result;
+                }
+            }
+            set { }
+        }
+
+        private void ConditionalNotifyAll() {
+            if (_queue.Count > 0)
+                Monitor.PulseAll(_lock);
+        }
+    }
+}
---
+++ Serie1/Serie1Tests/RetryLazyTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serie1;
+
+namespace Serie1Tests {
+
+
+    [TestClass]
+    public class RetryLazyTests {
+        private LinkedList<Exception> _exList;
+        private RetryLazy<string> _lazy;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _exList = new LinkedList<Exception>();
+        }
+
+        /*
+		|--------------------------------------------------------------------------
+		| Test Happy Path for one fetch
+		|--------------------------------------------------------------------------
+		*/
+        [TestMethod]
+        public void Test_SingleFetch()
+        {
+            _lazy = new RetryLazy<string>(() => "done", 1);
+
+            Assert.AreSame("done", _lazy.Value);
+        }
+
+        /*
+		|--------------------------------------------------------------------------
+		| Test No more retries to fetch, should throw exceptions
+		|--------------------------------------------------------------------------
+		*/
+        [TestMethod]
+        public void Test_NoMoreRetries() {
+            int retries = 3;
+            _lazy = new RetryLazy<string>(() => { throw new Exception("Yo, i'm out."); }, retries);
+
+            for (int i = 0; i < retries; i++) {
+                try {
+                    var res = _lazy.Value;
+                } catch (Exception e) {
+                    Assert.AreEqual(e.Message, "Yo, i'm out.");
+                    _exList.AddLast(e);
+                }
+            }
+
+            Assert.AreEqual(retries, _exList.Count);
+
+            try {
+                var r = _lazy.Value;
+                Assert.Fail("InvalidOperationException expected!");
+            } catch (Exception e) {
+                Assert.AreEqual(typeof(InvalidOperationException), e.GetType());
+            }
+        }
+
+        /*
+		|--------------------------------------------------------------------------
+		| Test Multiple Threads (with delay)
+		|--------------------------------------------------------------------------
+		*/
+        [TestMethod]
+        public void Test_MultipleThreadsWithSomeDelay()
+        {
+            int threads = 50;
+            int count = 0;
+            int retries = 10;
+            _lazy = new RetryLazy<string>(() => {
+                Thread.Sleep(200);
+
+                if (count++ < retries) // first tries should fail
+                    throw new Exception("I'm mad..");
+
+                return "done";
+            }, retries+1);
+
+
+            List<Thread> thList = new List<Thread>(threads);
+            for (int i = 0; i < threads; i++)
+            {
+                thList.Add(new Thread(() => {
+                    try
+                    {
+                        Assert.AreEqual("done", _lazy.Value);
+                    }
+                    catch (Exception e)
+                    {
+                        _exList.AddLast(e);
+                    }
+                }));
+            }
+
+            thList.ForEach((t) => t.Start());
+            thList.ForEach((t) => t.Join());
+
+            Assert.AreEqual(retries, _exList.Count);
+        }
+    }
+}
