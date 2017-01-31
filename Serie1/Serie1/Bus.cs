d20f44130f38b6670d7d2169a84dacea5900684b
Carlos Florencio <carlosmflorencio@gmail.com>
2017-01-31 15:46:32 +0000

    Added Bus

---
+++ Serie1/Serie1/Bus.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Serie1 {
+
+    public class Bus {
+
+        private readonly int _maxPending;
+        private bool shutdown = false;
+        private readonly object shutdownLock = new object();
+        private Dictionary<Type, LinkedList<object>> map = new Dictionary<Type, LinkedList<object>>();
+        private readonly object _lock = new object();
+
+        public Bus(int maxPending) {
+            this._maxPending = maxPending;
+        }
+
+        /*
+		|--------------------------------------------------------------------------
+		| Subscribe
+		|--------------------------------------------------------------------------
+		*/
+
+        public void SubscribeEvent<T>(Action<T> handler) where T : class {
+            bool lockTaken = false;
+            try {
+                Monitor.Enter(_lock, ref lockTaken);
+                LinkedListNode<object> node = GetOrCreateEventHandler(handler);
+
+                do {
+                    try {
+                        SyncUtils.Wait(_lock, node.Value);
+
+                        var myEvent = node.Value as Event<T>;
+
+                        while (myEvent.Messages.First != null) {
+                            var message = myEvent.Messages.First.Value;
+                            lockTaken = false;
+                            Monitor.Exit(_lock);
+
+                            myEvent.Handler(message); // execute outside of the lock
+
+                            Monitor.Enter(_lock, ref lockTaken);
+
+                            myEvent.Messages.RemoveFirst();
+                        }
+
+                        if (shutdown) {
+                            RemoveHandlerFromEventsList<T>(typeof(T), node.Value);
+
+                            if (map.Count == 0) { // last handler?
+                                SyncUtils.Notify(_lock, shutdownLock);
+                            }
+
+                            break;
+                        }
+
+                    } catch (ThreadInterruptedException e) {
+                        // remove handler from list
+                        RemoveHandlerFromEventsList<T>(typeof(T), node.Value);
+
+                        if (shutdown && map.Count == 0)
+                        { // last handler?
+                            SyncUtils.Notify(_lock, shutdownLock);
+                        }
+
+                        throw;
+                    }
+                } while (true);
+            } finally {
+                if (lockTaken) Monitor.Exit(_lock);
+            }
+
+        }
+
+        /*
+		|--------------------------------------------------------------------------
+		| Publish
+		|--------------------------------------------------------------------------
+		*/
+
+        public void PublishEvent<T>(T message) where T : class {
+            lock (_lock) {
+                if (shutdown)
+                    throw new InvalidOperationException();
+
+                LinkedList<object> events = null;
+                if (map.TryGetValue(typeof(T), out events) == false)
+                    return; // we dont have handlers yet, ignore
+
+                foreach (var e in events)
+                {
+                    var singleEvent = e as Event<T>;
+
+                    if (singleEvent.Messages.Count > _maxPending)
+                        continue; // no space to save the message
+
+                    singleEvent.Messages.AddLast(message);
+                    SyncUtils.Notify(_lock, singleEvent); // notify the subscribed thread
+                }
+            }
+        }
+
+        /*
+		|--------------------------------------------------------------------------
+		| Shutdown
+		|--------------------------------------------------------------------------
+		*/
+
+        public void Shutdown() {
+            lock (_lock) {
+                if(shutdown) return;
+
+                shutdown = true;
+
+                foreach (KeyValuePair<Type, LinkedList<object>> entry in map)
+                {
+                    foreach (var o in entry.Value) { // notify all thread handlers
+                        SyncUtils.Notify(_lock, o);
+                    }
+                }
+
+                try {
+                    if(map.Count > 0)
+                        SyncUtils.Wait(_lock, shutdownLock);
+                } catch (ThreadInterruptedException e) {
+                    throw e;
+                }
+            }
+        }
+
+        private LinkedListNode<object> GetOrCreateEventHandler<T>(Action<T> action)
+        {
+            LinkedList<object> events = null;
+            if (map.TryGetValue(typeof(T), out events) == false)
+            { // create the Event
+                events = new LinkedList<object>();
+                map.Add(typeof(T), events);
+                return events.AddLast(new Event<T>(action));
+            }
+
+            return events.AddLast(new Event<T>(action));
+        }
+
+        private void RemoveHandlerFromEventsList<T>(Type key, object handler) {
+            map[key].Remove(handler);
+
+            if (map[key].Count == 0)
+                map.Remove(key);
+        }
+
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Inner types
+    |--------------------------------------------------------------------------
+    */
+
+    class Event<T> {
+
+        public Event(Action<T> h) {
+            this.Handler = h;
+            this.Messages = new LinkedList<T>();
+        }
+
+        public Action<T> Handler;
+        public LinkedList<T> Messages;
+
+    }
+
+}
---
+++ Serie1/Serie1Tests/BusTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serie1;
+
+namespace Serie1Tests
+{
+    [TestClass]
+    public class BusTests
+    {
+        /*
+		|--------------------------------------------------------------------------
+		| Test Happy Path
+		|--------------------------------------------------------------------------
+		*/
+
+        [TestMethod]
+        public void Test_HappyPath()
+        {
+            var bus = new Bus(1);
+            var msgCompare = "Teste";
+
+            Thread t1 = new Thread(() =>
+            {
+                bus.SubscribeEvent<String>((msg) =>
+                {
+                    Assert.AreEqual(msgCompare, msg);
+                });
+            });
+
+            t1.Start();
+            Thread.Sleep(100); // wait to subscribe
+            bus.PublishEvent(msgCompare);
+
+            bus.Shutdown();
+            t1.Join();
+        }
+
+        class TestType {
+
+            public int value;
+
+            public TestType(int val) {
+                this.value = val;
+            }
+
+        }
+
+        [TestMethod]
+        public void Test_MultipleTypes()
+        {
+            var bus = new Bus(1);
+            var msgCompare = "Teste";
+            var typeCompare = new TestType(2);
+            int run = 0;
+            Thread t1 = new Thread(() =>
+            {
+                bus.SubscribeEvent<String>((msg) =>
+                {
+                    Assert.AreEqual(msgCompare, msg);
+                    run++;
+                });
+            });
+
+            Thread t2 = new Thread(() =>
+            {
+                bus.SubscribeEvent<TestType>((type) =>
+                {
+                    Assert.AreEqual(2, type.value);
+                    run++;
+                });
+            });
+
+            t1.Start();
+            t2.Start();
+            Thread.Sleep(100); // wait for subscribes
+            bus.PublishEvent(msgCompare);
+            bus.PublishEvent(typeCompare);
+
+            bus.Shutdown();
+            t1.Join();
+            t2.Join();
+
+            Assert.AreEqual(2, run);
+        }
+    }
+}
