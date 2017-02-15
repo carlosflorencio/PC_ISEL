f3ca9473d366fdf16f601d9d60d3b7147458cf29
Carlos Florencio <carlosmflorencio@gmail.com>
2017-02-15 15:54:46 +0000

    Started fileui

---
+++ Serie3/FileSearch/FileSearch.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileSearch {
+
+    class SearchResult {
+
+        public int totalFiles; // updated with interlocked
+        public int totalFilesWithExtension; // updated with interlocked
+        public ConcurrentBag<String> files = new ConcurrentBag<string>();
+
+    }
+
+    class FileSearch {
+
+        // Caller must catch the AggregateException
+        public static Task<SearchResult> Find(string folder, string ext,
+            string sequence, CancellationToken token) {
+            var task = Task.Factory.StartNew<SearchResult>(() => {
+                var result = new SearchResult();
+
+                // get all the files in that directory to count
+                var files = Directory.GetFiles(folder);
+
+                result.totalFiles = files.Length; // save the count
+
+                // filter the files by the extension we want (toArray because we want the size)
+                var filesWithExtension = files.Where(f => f.EndsWith("." + ext)).ToArray();
+                result.totalFilesWithExtension = filesWithExtension.Length;
+
+                // if canceled at this point, we dont need to read all the files
+                token.ThrowIfCancellationRequested();
+
+                // used to throttled the IO concurrency in case we have many files
+                SemaphoreSlim semaphore = new SemaphoreSlim(20);
+
+                // read the contents of all the files
+                // we assume there are multiple cores on this system
+                // if not, we should do a sync version
+                Parallel.ForEach(filesWithExtension, (file, loop) => {
+                    semaphore.Wait(token); // cancel the wait if token is signaled
+
+                    using (StreamReader reader = File.OpenText(file)) {
+                        string line = null;
+                        while ((line = reader.ReadLine()) != null) {
+                            if (line.Contains(sequence)) {
+                                result.files.Add(file);
+                                break;
+                            }
+                        }
+                    }
+
+                    if (token.IsCancellationRequested) {
+                        loop.Stop(); // cancell all the remaining loop tasks
+                    }
+
+                    semaphore.Release();
+                });
+
+
+                return result;
+            }, token);
+
+            return task;
+        }
+
+    }
+
+}
---
+++ Serie3/FileSearchUI/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSearchUI {
+
+    public partial class Form1 : Form {
+
+        private string folder = "";
+        private string ext = "";
+        private string query = "";
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+        }
+
+
+        private void button1_Click(object sender, EventArgs e) {
+            var result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK) {
+                folder = folderBrowserDialog1.SelectedPath;
+
+
+                textBox1.Text = folder;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (thereIsValidInformation() == false)
+                return;
+        }
+
+        private bool thereIsValidInformation() {
+            folder = textBox1.Text;
+            ext = textBox2.Text;
+            query = textBox3.Text;
+
+            if (folder.Length == 0) {
+                MessageBox.Show("A folder is necessary.");
+                return false;
+            }
+
+            if (!Directory.Exists(folder)) {
+                MessageBox.Show("That folder does not exist!");
+                return false;
+            }
+
+            if (ext.Length == 0) {
+                MessageBox.Show("File extension should be provided.");
+                return false;
+            }
+
+            if (query.Length == 0) {
+                MessageBox.Show("A word to search is necessary.");
+                return false;
+            }
+
+            // just in case..
+            ext = ext.Replace(".", "");
+
+            return true;
+        }
+
+    }
+
+}
